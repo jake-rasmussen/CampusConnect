993d80509e778617cda8a74ba1763272bcc07663
jrasmus6 <89819505+jrasmus6@users.noreply.github.com>
2023-12-12 15:27:02 -0500

    protected the routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,22 +5,22 @@ export default authMiddleware({
   afterAuth(auth, req, evt) {
     const url = new URL(req.url);
 
+    const publicMetadata = auth.sessionClaims?.publicMetadata as {
+      adminProjectIds: string;
+      evaluatorProjectIds: string;
+    };
+
+    const evaluatorProjectIds: string[] = JSON.parse(
+      publicMetadata.evaluatorProjectIds,
+    );
+    const adminProjectIds: string[] = JSON.parse(
+      publicMetadata.adminProjectIds,
+    );
+
     if (!auth.userId && !auth.isPublicRoute) {
       return redirectToSignIn({ returnBackUrl: req.url });
     } else if (url.pathname.includes("/admin/")) {
       const { pathname } = url;
-
-      const publicMetadata = auth.sessionClaims?.publicMetadata as {
-        adminProjectIds: string;
-        evaluatorProjectIds: string;
-      };
-
-      const evaluatorProjectIds: string[] = JSON.parse(
-        publicMetadata.evaluatorProjectIds,
-      );
-      const adminProjectIds: string[] = JSON.parse(
-        publicMetadata.adminProjectIds,
-      );
 
       const parts = pathname.split("/");
 
@@ -28,6 +28,18 @@ export default authMiddleware({
       if (adminIndex !== -1 && adminIndex < parts.length - 1) {
         const projectId = parts[adminIndex + 1];
         if (projectId && !adminProjectIds.includes(projectId)) {
+          return redirectToSignIn({ returnBackUrl: req.url });
+        }
+      }
+    } else if (url.pathname.includes("evaluator/")) {
+      const { pathname } = url;
+
+      const parts = pathname.split("/");
+
+      const evaluatorIndex = parts.indexOf("evaluator");
+      if (evaluatorIndex !== -1 && evaluatorIndex < parts.length - 1) {
+        const projectId = parts[evaluatorIndex + 1];
+        if (projectId && !evaluatorProjectIds.includes(projectId)) {
           return redirectToSignIn({ returnBackUrl: req.url });
         }
       }
