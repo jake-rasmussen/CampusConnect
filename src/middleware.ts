6c1ca649eb04e2500e4abe6c24dcd752bafea8ee
jrasmus6 <89819505+jrasmus6@users.noreply.github.com>
2023-12-18 10:59:08 -0500

    fixed afterAuth not redirecting to sign in

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,39 +10,43 @@ export default authMiddleware({
       evaluatorProjectIds: string;
     };
 
-    const evaluatorProjectIds: string[] = JSON.parse(
-      publicMetadata.evaluatorProjectIds,
-    );
-    const adminProjectIds: string[] = JSON.parse(
-      publicMetadata.adminProjectIds,
-    );
+    if (publicMetadata) {
+      const evaluatorProjectIds: string[] = JSON.parse(
+        publicMetadata.evaluatorProjectIds,
+      );
+      const adminProjectIds: string[] = JSON.parse(
+        publicMetadata.adminProjectIds,
+      );
 
-    if (!auth.userId && !auth.isPublicRoute) {
+      if (!auth.userId && !auth.isPublicRoute) {
+        return redirectToSignIn({ returnBackUrl: req.url });
+      } else if (url.pathname.includes("/admin/")) {
+        const { pathname } = url;
+
+        const parts = pathname.split("/");
+
+        const adminIndex = parts.indexOf("admin");
+        if (adminIndex !== -1 && adminIndex < parts.length - 1) {
+          const projectId = parts[adminIndex + 1];
+          if (projectId && !adminProjectIds.includes(projectId)) {
+            return redirectToSignIn({ returnBackUrl: req.url });
+          }
+        }
+      } else if (url.pathname.includes("evaluator/")) {
+        const { pathname } = url;
+
+        const parts = pathname.split("/");
+
+        const evaluatorIndex = parts.indexOf("evaluator");
+        if (evaluatorIndex !== -1 && evaluatorIndex < parts.length - 1) {
+          const projectId = parts[evaluatorIndex + 1];
+          if (projectId && !evaluatorProjectIds.includes(projectId)) {
+            return redirectToSignIn({ returnBackUrl: req.url });
+          }
+        }
+      }
+    } else {
       return redirectToSignIn({ returnBackUrl: req.url });
-    } else if (url.pathname.includes("/admin/")) {
-      const { pathname } = url;
-
-      const parts = pathname.split("/");
-
-      const adminIndex = parts.indexOf("admin");
-      if (adminIndex !== -1 && adminIndex < parts.length - 1) {
-        const projectId = parts[adminIndex + 1];
-        if (projectId && !adminProjectIds.includes(projectId)) {
-          return redirectToSignIn({ returnBackUrl: req.url });
-        }
-      }
-    } else if (url.pathname.includes("evaluator/")) {
-      const { pathname } = url;
-
-      const parts = pathname.split("/");
-
-      const evaluatorIndex = parts.indexOf("evaluator");
-      if (evaluatorIndex !== -1 && evaluatorIndex < parts.length - 1) {
-        const projectId = parts[evaluatorIndex + 1];
-        if (projectId && !evaluatorProjectIds.includes(projectId)) {
-          return redirectToSignIn({ returnBackUrl: req.url });
-        }
-      }
     }
   },
 });
